e644104d353086706a906b89fdccfb2ec2b27f48
Naoto Ida <naoto.ida+programming@gmail.com>
2020-01-11 13:18:35 +0100

    Initial Commit

---
+++ TouchbarAudioSwitcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchbarAudioSwitcher
+//
+//  Created by Nao on 11.01.20.
+//  Copyright © 2020 Naoto Ida. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
